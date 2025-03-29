45da95bb49c4b885d4a84dc7d468df611936d571
lilacGalaxy <ajcarley147@gmail.com>
2025-03-28 20:35:30 -0700

    Apply formatting

--- compute_params_from_blendshape.py
+++ compute_params_from_blendshape.py
@@ -2,86 +2,108 @@ import math
 
 BLINK_THRESHOLD = 0.7
 
+
 def append_request(request, id, value):
     request["data"]["parameterValues"].append({"id": id, "value": value})
 
+
 def get_mouth_smile(blendshapes):
     smile = max(blendshapes["mouthSmileLeft"], blendshapes["mouthSmileRight"])
-    frown = max(blendshapes["mouthFrownLeft"], blendshapes["mouthFrownRight"])
-    return (smile)*.6 + .4
+    frown = max(
+        blendshapes["mouthFrownLeft"], blendshapes["mouthFrownRight"]
+    )  # not getting values from this
+    return (smile) * 0.6 + 0.4
+
 
 def get_mouth_open(blendshapes):
     return math.sqrt(blendshapes["jawOpen"])
+
 
 def get_mouth_x(blendshapes):
     left = blendshapes["mouthLeft"]
     right = blendshapes["mouthRight"]
     return right - left
 
+
 def get_brows(blendshapes):
     brows_down = max(blendshapes["browDownLeft"], blendshapes["browDownRight"])
-    brows_up = max(max(blendshapes["browInnerUp"], blendshapes["browOuterUpLeft"]), blendshapes["browOuterUpRight"])
+    brows_up = max(
+        max(blendshapes["browInnerUp"], blendshapes["browOuterUpLeft"]),
+        blendshapes["browOuterUpRight"],
+    )
     return brows_up - brows_down
+
 
 def get_brows_left_y(blendshapes):
     brows_down = blendshapes["browDownLeft"]
     brows_up = max(blendshapes["browInnerUp"], blendshapes["browOuterUpLeft"])
     return brows_up - brows_down
 
+
 def get_brows_right_y(blendshapes):
     brows_down = blendshapes["browDownRight"]
     brows_up = max(blendshapes["browInnerUp"], blendshapes["browOuterUpRight"])
     return brows_up - brows_down
 
+
 def get_brows_left_form(blendshapes):
-    brow_inner = blendshapes["browInnerUp"] 
+    brow_inner = blendshapes["browInnerUp"]
     brow_outer = blendshapes["browOuterUpLeft"]
     return brow_inner - brow_outer
 
+
 def get_brows_right_form(blendshapes):
-    brow_inner = blendshapes["browInnerUp"] 
+    brow_inner = blendshapes["browInnerUp"]
     brow_outer = blendshapes["browOuterUpRight"]
     return brow_inner - brow_outer
+
 
 def get_eye_open_left(blendshapes):
     squint = blendshapes["eyeSquintLeft"]
     blink = blendshapes["eyeBlinkLeft"]
-    if (blink > BLINK_THRESHOLD):
-        return (1 - blink) * .5
-    return min(1.9*(1 - squint),1)
+    if blink > BLINK_THRESHOLD:
+        return (1 - blink) * 0.5
+    return min(1.9 * (1 - squint), 1)
+
 
 def get_eye_open_right(blendshapes):
     squint = blendshapes["eyeSquintRight"]
     blink = blendshapes["eyeBlinkRight"]
-    if (blink > BLINK_THRESHOLD):
-        return (1 - blink) * .5
-    return min(1.9*(1 - squint),1)
+    if blink > BLINK_THRESHOLD:
+        return (1 - blink) * 0.5
+    return min(1.9 * (1 - squint), 1)
+
 
 def get_eye_left_x(blendshapes):
     eye_left = blendshapes["eyeLookOutLeft"]
     eye_right = blendshapes["eyeLookInRight"]
     return eye_left - eye_right
 
+
 def get_eye_left_y(blendshapes):
     eye_up = blendshapes["eyeLookUpLeft"]
     eye_down = blendshapes["eyeLookDownLeft"]
     return eye_up - eye_down
+
 
 def get_eye_right_x(blendshapes):
     eye_right = blendshapes["eyeLookOutRight"]
     eye_left = blendshapes["eyeLookInRight"]
     return eye_left - eye_right
 
+
 def get_eye_right_y(blendshapes):
     eye_up = blendshapes["eyeLookUpRight"]
     eye_down = blendshapes["eyeLookDownRight"]
     return eye_up - eye_down
+
 
 def create_blendshapes_dict(blendshape_list):
     shapes = {}
     for shape in blendshape_list:
         shapes[shape.category_name] = shape.score
     return shapes
+
 
 def compute_params_from_blendshapes(request, blendshape_list):
     blendshapes = create_blendshapes_dict(blendshape_list)
@@ -117,4 +139,3 @@ def compute_params_from_blendshapes(request, blendshape_list):
     append_request(request, "lilac_BrowsLeftForm", get_brows_left_form(blendshapes))
     # lilac_BrowsRightForm
     append_request(request, "lilac_BrowsRightForm", get_brows_right_form(blendshapes))
-
--- create_parameters.py
+++ create_parameters.py
@@ -3,51 +3,78 @@ import os
 from websockets.sync.client import connect
 from vtube_studio_interface import get_authentication_token, vtube_studio_authenticate
 
-def parameter_creation_request(parameter_name, explanation, min_val=0, max_val=1, default_val=0):
+
+def parameter_creation_request(
+    parameter_name, explanation, min_val=0, max_val=1, default_val=0
+):
     message = {
-        "apiName":"VTubeStudioPublicAPI", 
-        "apiVersion":"1.0", 
-        "requestID":"lilacsMediaPipeForward", 
-        "messageType":"ParameterCreationRequest",
+        "apiName": "VTubeStudioPublicAPI",
+        "apiVersion": "1.0",
+        "requestID": "lilacsMediaPipeForward",
+        "messageType": "ParameterCreationRequest",
         "data": {
             "parameterName": parameter_name,
             "explanation": explanation,
             "min": min_val,
             "max": max_val,
-            "defaultValue": default_val
-        }
+            "defaultValue": default_val,
+        },
     }
     return json.dumps(message)
 
+
 def create_parameter(websocket, name, description, min_val, max_val, default_val):
-        request_message_json = parameter_creation_request(name, description, min_val, max_val, default_val)
-        websocket.send(request_message_json)
-        response_json = websocket.recv()
-        print(json.loads(response_json))
+    request_message_json = parameter_creation_request(
+        name, description, min_val, max_val, default_val
+    )
+    websocket.send(request_message_json)
+    response_json = websocket.recv()
+    print(json.loads(response_json))
+
 
 def create_custom_parameters(auth_token=""):
 
     with connect("ws://localhost:8001") as websocket:
         # authenticate session
         try:
-            if (auth_token == ""):
+            if auth_token == "":
                 auth_token = get_authentication_token(websocket)
             vtube_studio_authenticate(websocket, auth_token)
         except:
             print("Unable to authorize")
             exit(1)
-        
+
         # Now authenticated, add parameters
-        create_parameter(websocket, "lilac_MouthX", "mediapipe mouthX", min_val=-1, max_val=1, default_val=0)
-        create_parameter(websocket, "lilac_BrowsLeftForm", "mediapipe mouthX", min_val=-1, max_val=1, default_val=0)
-        create_parameter("lilac_BrowsRightForm", "mediapipe mouthX", min_val=-1, max_val=1, default_val=0)
+        create_parameter(
+            websocket,
+            "lilac_MouthX",
+            "mediapipe mouthX",
+            min_val=-1,
+            max_val=1,
+            default_val=0,
+        )
+        create_parameter(
+            websocket,
+            "lilac_BrowsLeftForm",
+            "mediapipe mouthX",
+            min_val=-1,
+            max_val=1,
+            default_val=0,
+        )
+        create_parameter(
+            "lilac_BrowsRightForm",
+            "mediapipe mouthX",
+            min_val=-1,
+            max_val=1,
+            default_val=0,
+        )
         return auth_token
-    
+
 
 if __name__ == "__main__":
     auth_token = ""
-    if (os.path.isfile("auth.json")):
-        with open("auth.json", 'r') as auth_file:
+    if os.path.isfile("auth.json"):
+        with open("auth.json", "r") as auth_file:
             auth_data = json.load(auth_file)
             auth_token = auth_data["auth_token"]
     create_custom_parameters(auth_token)
